90d78b3d801dfa6642e4c8451e18ea0234e1a388
daenamcclintock <daena.mcclintock@gmail.com>
2022-06-27 09:06:17 -0400

    added tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import { MoralisProvider } from "react-moralis"
+import Header from '../components/Header'
 
 function MyApp({ Component, pageProps }) {
   return (
     <MoralisProvider initializeOnMount={false}>
+      <Header />
       <Component {...pageProps} />
     </MoralisProvider>
   )
