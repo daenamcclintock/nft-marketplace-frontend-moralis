f719c64bef94d619c0b527aa9d53e74f6a1865f0
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-11 09:31:52 -0400

    minor updates

--- pages/_app.js
+++ pages/_app.js
@@ -8,21 +8,21 @@ const APP_ID = process.env.NEXT_PUBLIC_APP_ID
 const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-        <Head>
-            <title>NFT Marketplace</title>
-            <meta name="description" content="NFT Marketplace" />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-            <NotificationProvider>
-                <Header />
-                <Component {...pageProps} />
-            </NotificationProvider>
-        </MoralisProvider>
-    </div>
-) 
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
+                <NotificationProvider>
+                    <Header />
+                    <Component {...pageProps} />
+                </NotificationProvider>
+            </MoralisProvider>
+        </div>
+    ) 
 }
 
 export default MyApp
